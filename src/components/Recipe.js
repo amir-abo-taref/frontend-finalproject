3b5f0193e99d1e2755314908d3706d3a8d5541f3
unknown <you@example.com>
2021-12-12 14:27:08 +0200

    some msg

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState} from 'react';
 import "./Recipe.css";
 import axios from "axios";
-
-const RecipeApp = () => {
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+const RecipeApp = ({ auth: { user } }) => {
     const [meal, setMeal] = useState([]);
     const [recipe,setRecipe]=useState({
         name:'',
@@ -51,16 +52,19 @@ const RecipeApp = () => {
             <div className="recipe-page">
                 <div className="input-recipe">
                     <div className="recipe-header1">
+                    <div className="recipe-text"> enter your name</div>
+<input className="recipe-inputbtn" maxLength={9} type="text"  name="username" value={recipe.username} onChange={inputHandler} placeholder="your name" />
 <div className="recipe-text"> your food name</div>
-<input className="recipe-inputbtn" type="text"  name="name" value={recipe.name} onChange={inputHandler} placeholder="Food name" />
+<input className="recipe-inputbtn"  maxLength={9} type="text"  name="name" value={recipe.name} onChange={inputHandler} placeholder="Food name" />
 <div className="recipe-text">your food image</div>
 <input className="recipe-inputbtn" type="url" name="image" value={recipe.image} onChange={inputHandler} placeholder="Food image-(url)" />
 <div className="recipe-text">your food caloriot</div>
-<input className="recipe-inputbtn" type="number"  name="caloriot" value={recipe.caloriot} onChange={inputHandler} placeholder="caloriot" />
+<input className="recipe-inputbtn"  type="number"  name="caloriot" value={recipe.caloriot} onChange={inputHandler} placeholder="caloriot" />
 <div className="recipe-text">time to make</div>
-<input className="recipe-inputbtn" type="number"  name="time" value={recipe.time} onChange={inputHandler} placeholder="time" />
+<input className="recipe-inputbtn"   type="number"  name="time" value={recipe.time} onChange={inputHandler} placeholder="time" />
 <div className="recipe-text">click here to add your recipe</div>
 <input className="recipe-btn" type="submit" name="add food" value="add Your Food" onClick={postdata}  />
+<h6 className="madebyuser" >Made by, {user&&user.name}</h6>
 </div>
 <div className="recipe-header2">
 <div className="recipe-text">Tap here how to make your food </div>
@@ -76,6 +80,7 @@ const RecipeApp = () => {
           <div className="recipeheader" >{e.name}</div>
           <div className="recipetext">calories:({e.caloriot})grams</div>
           <div className="recipetext">Time:({e.time})mins</div>
+          <div className="recipetextbutton">Made by:{e.username}</div>
           <div className="overlay">
           <div className="recipetextinfo"><div className="infoingtext">Ingredients</div>({e.info})</div> 
           </div>
@@ -88,4 +93,12 @@ const RecipeApp = () => {
         )  
 }
 
-export default RecipeApp;
+RecipeApp.propTypes = {
+	auth: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+	auth: state.auth,
+});
+
+export default connect(mapStateToProps)(RecipeApp);
